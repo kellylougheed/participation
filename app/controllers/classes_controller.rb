9e3ba5fb7b2113367795d958fd2a7fcb5605aaae
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-07 18:41:27 +0000

    Add classes dashboard

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,11 +1,11 @@
 class ClassesController < ApplicationController
 
   def index
-    @classes = Class.all
+    @courses = Course.all
   end
 
   def new
-    @class = Class.new
+    @course = Course.new
   end
   
 end
