d7e622381f29bf53bc5f2c0f2866939b6edc794c
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-07 19:10:53 +0000

    Made new class form functional

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,4 +1,5 @@
 class ClassesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @courses = Course.all
@@ -6,6 +7,17 @@ class ClassesController < ApplicationController
 
   def new
     @course = Course.new
+  end
+
+  def create
+    Course.create(course_params)
+    redirect_to classes_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :user_id)
   end
   
 end
