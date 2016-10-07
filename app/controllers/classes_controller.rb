5b037d641e1892368015a906caf00f639d7f202c
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-07 18:30:28 +0000

    Add courses model

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,6 +1,11 @@
 class ClassesController < ApplicationController
 
   def index
+    @classes = Class.all
+  end
+
+  def new
+    @class = Class.new
   end
   
 end
---
+++ db/migrate/20161007181805_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :user_id
+      t.timestamps
+    end
+
+    add_index :courses, :user_id
+  end
+end
