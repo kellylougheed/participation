113e79131f6c90b4da3956c21d5330e3dc3959ef
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-08 00:01:37 +0000

    Put new class form in modal, add accordion for classes, style navbar and buttons

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,12 +1,7 @@
 class ClassesController < ApplicationController
-  before_action :authenticate_user!
 
   def index
     @courses = Course.all
-  end
-
-  def new
-    @course = Course.new
   end
 
   def create
