72aba59e77b2c7adcf2d9369b9aac5aaf69888fb
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-15 20:11:02 +0000

    Finish script to add or subtract points on click

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,10 +7,16 @@ class StudentsController < ApplicationController
     redirect_to class_path(@course)
   end
 
+  def update
+    Student.find(params[:id]).update_attributes(student_params)
+
+    redirect_to class_path(@course)
+  end
+
   private
 
   def student_params
-    params.require(:student).permit(:first_name, :last_name)
+    params.require(:student).permit(:first_name, :last_name, :points)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :classes do
-    resources :students
+    resources :students, only: :create
   end
+
+  resources :students, only: :update
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
