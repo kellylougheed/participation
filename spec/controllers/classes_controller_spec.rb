d95eb914bdbccc76fc5960621f2b01787338b0f3
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-19 22:22:51 +0000

    Add tests for classes#index and classes#show

--- spec/controllers/classes_controller_spec.rb
+++ spec/controllers/classes_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe ClassesController, type: :controller do
 
+  describe "classes#index action" do
+    it "should successfully show the page" do
+      u = FactoryGirl.create(:user, email: 'teacher@teachers.com', password: 'ILOVESCHOOL')
+      sign_in u
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "classes#show action" do
+    it "should successfully show the class if the id exists" do
+      u = FactoryGirl.create(:user, email: 'teacher@teachers.com', password: 'ILOVESCHOOL')
+      sign_in u
+      course = FactoryGirl.create(:course, user_id: u.id)
+      get :show, id: course.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,23 @@
 FactoryGirl.define do
   factory :user do
+
   end
-  
+
   factory :course do
+    name "Latin"
+    starting_points 50
+    total_points 75
   end
 
   factory :student do
+    first_name "Lily"
+    last_name "Ross Taylor"
+    association :course
   end
 
   factory :comment do
+    message "Good contribution"
+    association :student
   end
 
 end
