ef1dc053ac9b4a7c9bec550ad4ef3f379218b446
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-21 05:14:05 +0000

    Change front page pictures

--- spec/controllers/classes_controller_spec.rb
+++ spec/controllers/classes_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ClassesController, type: :controller do
 
   describe "classes#index action" do
-    it "should successfully show the page" do
+    it "should successfully show classes to a logged-in teacher" do
       u = FactoryGirl.create(:user, email: 'teacher@teachers.com', password: 'ILOVESCHOOL')
       sign_in u
       get :index
@@ -12,7 +12,7 @@ RSpec.describe ClassesController, type: :controller do
   end
 
   describe "classes#show action" do
-    it "should successfully show the class if the id exists" do
+    it "should successfully show the class if the id exists to the teacher" do
       u = FactoryGirl.create(:user, email: 'teacher@teachers.com', password: 'ILOVESCHOOL')
       sign_in u
       course = FactoryGirl.create(:course, user_id: u.id)
