7b6921a8317359e7260e33fd55ef22ad5129ab7b
Kelly Lougheed <kellylougheed@gmail.com>
2016-12-31 04:23:34 +0000

    Edit validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
   belongs_to :course
   validates :first_name, presence: true, length: { minimum: 1 }
-  validates :last_name, presence: true, length: { minimum: 1 }
+  validates :last_name, presence: true
   has_many :comments
 end
